0203e323e1e723c11734d9fd6bd4871506f56be3
Siti Nabilah <sitinabilah0123@gmail.com>
2020-04-11 23:21:19 +0800

    widget background and photo

--- lib/editprofile.dart
+++ lib/editprofile.dart
@@ -8,4 +8,40 @@ class EditProfile extends StatefulWidget {
 
 class _ProfilePageState extends State<EditProfile> {
 
+    Widget _greenColors() {
+    return Positioned(
+      top: 0,
+      child: Container(
+        color: Color(0xff022264),
+        //height: 205,
+        width: MediaQuery.of(context).size.width,
+
+        padding: const EdgeInsets.all(14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                 CircleAvatar(
+                   backgroundImage: ExactAssetImage('assets/img/profile.png'),
+                   backgroundColor: Color(0xff3DBC93), 
+                    radius: 55,
+                  
+                )
+                                  
+                ],
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              
+
+              
+            ],
+          ),
+      ),
+    );
+  }
+
 }
