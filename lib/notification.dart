c3da713124b4c92b23909dde99f67583d90cac72
Nurul Ain <nurulain0897@gmail.com>
2020-04-12 00:43:53 +0800

    import class notification1

---
+++ lib/notification.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class Notification1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+ backgroundColor: Color(0xffECECEC),
+        appBar: AppBar()
+
+
+    );}}
