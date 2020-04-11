e31ccc3b19302fa24b5897f72aa1263e16f7a18a
Siti Nabilah <sitinabilah0123@gmail.com>
2020-04-11 23:12:00 +0800

    class HomeScreeen2

---
+++ lib/menu.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+//import 'package:map/notification.dart';
+
+class HomeScreen2 extends StatefulWidget {
+  
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+
+  
+}
+
+
+class _HomeScreenState extends State<HomeScreen2> {
+   int counter = 2;
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
