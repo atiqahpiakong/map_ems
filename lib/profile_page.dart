652cac0cb78ca56233720c04cc05f6258fbb9ac4
Siti Nabilah <sitinabilah0123@gmail.com>
2020-04-11 23:15:25 +0800

    class ProfilePage

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
 import 'editprofile.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+
+
+}
