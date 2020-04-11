c0fd024901d14e26ece6009fc6d1f43e62a0431b
Siti Nabilah <sitinabilah0123@gmail.com>
2020-04-11 23:16:19 +0800

    widget background, photo, name, position

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -8,5 +8,66 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
 
+    Widget _greenColors() {
+    return Positioned(
+      top: 0,
+      child: Container(
+        color: Color(0xff022264),
+        //height: 205,
+        width: MediaQuery.of(context).size.width,
 
+        padding: const EdgeInsets.all(14),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                 CircleAvatar(
+                   backgroundImage: ExactAssetImage('assets/img/profile.png'),
+                   backgroundColor: Color(0xff3DBC93), 
+                    radius: 60,
+                  
+                )
+                                  
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                  "Nurul Atiqah ", textAlign: TextAlign.center,
+                  
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                   
+                  ),
+                ),
+                ],
+              ),
+
+               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                  "Software Department ", textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.white,
+                    
+                    
+                  ),
+                ),
+                ],
+              ),
+            ],
+          ),
+      ),
+    );
+  }
 }
