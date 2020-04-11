d98e70fb93238a9dcecb0ea39deefa9406d05b7b
Siti Nabilah <sitinabilah0123@gmail.com>
2020-04-11 23:20:45 +0800

    import file editProfile

---
+++ lib/editprofile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class EditProfile extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<EditProfile> {
+
+}
