f15cd927a4d7d738cdcb38b590b0260e799e63a0
Siti Nabilah <sitinabilah0123@gmail.com>
2020-05-05 22:03:02 +0800

    Create mock data for employee

---
+++ lib/model/employee.dart
@@ -0,0 +1,15 @@
+class Employee {
+  String name; 
+  String dept;
+  String email;
+  String contact;
+  String address;
+  String status;
+
+  Employee(
+
+    {
+      this.name, this.dept, this.email, this.contact, this.address, this.status }
+      
+  );
+}
---
+++ lib/model/empmockdata.dart
@@ -0,0 +1,49 @@
+import 'employee.dart';
+
+List <Employee> mockListEmp = [
+  
+    Employee(
+        name: "Nurul Balqesh",
+        dept: "Software Department",
+        email: "nurulbalqesh@gmail.com",
+        contact: "0124689292",
+        address: "Ipoh, Perak",
+        status: "Employee"
+        
+        ),
+
+
+    Employee(
+        name: "Siti Nabilah",
+        dept: "Software Department",
+        email: "sitinabilah@gmail.com",
+        contact: "012833992",
+        address: "Pontian, Johor",
+        status: "Employee"
+
+        ),
+        
+    Employee(
+        name: "Nurul Atiqah",
+        dept: "Multimedia Department",
+        email: "atiqah@gmail.com",
+        contact: "0139689292",
+        address: "Kota Kinabalu, Sabah",
+        status: "Employee"
+
+        ),
+
+
+    Employee(
+        name: "Nurul Ain",
+        dept: "HR Department",
+        email: "ain@gmail.com",
+        contact: "0143689292",
+        address: "Rawang, Selangor",
+        status: "Employee"
+
+        ),
+
+    ];
+
+
