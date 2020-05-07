db740ed029632cfb724ef3fd9acd809dba15908a
Siti Nabilah <sitinabilah0123@gmail.com>
2020-05-07 15:55:19 +0800

    Create mockdata for leave

---
+++ lib/model/Leave.dart
@@ -0,0 +1,45 @@
+import 'myLeave.dart';
+
+List <Leave> mockLeave = [
+
+    Leave(
+        name: "Nurul Atiqah",
+        dateFrom: "3/10/2020",
+        dateTo: "4/10/2020",
+        status: "Pending",
+        type: "Sick",
+        reason: "Fever"),
+        
+    Leave(
+        name: "Nurul Atiqah",
+        dateFrom: "23/02/2020",
+        dateTo: "24/02/2020",
+        status: "Pending",
+        type: "Vacation",
+        reason: "Emergency"),
+        
+    Leave(
+        name: "Nurul Atiqah",
+        dateFrom: "3/10/2019",
+        dateTo: "4/10/2019",
+        status: "Pending",
+        type: "Holiday",
+        reason: "friends"),
+
+    Leave(
+        name: "Nurul Atiqah",
+        dateFrom: "1/10/2019",
+        dateTo: "2/10/2019",
+        status: "Rejected",
+        type: "Holiday",
+        reason: "Balik Kampung"),
+
+     Leave(
+
+        name: "Siti Nabilah",
+        dateFrom: "1/09/2019",
+        dateTo: "4/09/2019",
+        status: "Approved",
+        type: "Married",
+        reason: "Balik Kampung"),
+  ];
---
+++ lib/model/myLeave.dart
@@ -0,0 +1,11 @@
+class Leave {
+  String name;
+  String dateFrom;
+  String dateTo;
+  String status;
+  String type;
+  String reason;
+
+  Leave(
+      {this.name, this.dateFrom, this.dateTo, this.status, this.type, this.reason});
+}
