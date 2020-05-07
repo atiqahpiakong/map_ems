1416b0cbb69fb05e176a3432d5c5fb1709fd33cf
atiqah <atiqahpkg@gmail.com>
2020-05-07 14:59:59 +0800

    fixed navigation bar for admin

---
+++ lib/homescreen_admin.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'menu_admin.dart';
+import 'home.dart';
+import 'profile_page.dart';
+import 'menu.dart';
+import 'package:emsproject/model/empmockdata.dart';
+
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'homescreen_admin.dart';
+
+class AdminNavBar extends StatefulWidget {
+  @override
+  _AdminNavBar createState() => _AdminNavBar();
+}
+
+class _AdminNavBar extends State<AdminNavBar> {
+  int i = 0;
+  var pages = [
+    new HomeScreen(),
+    new AdminMenu(),
+    new ProfilePage(mockListEmp[2]),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: pages[i],
+      bottomNavigationBar: new CurvedNavigationBar(
+        height: 50.0,
+        items: <Widget>[
+          Icon(
+            Icons.home,
+            size: 30,
+          ),
+          Icon(Icons.menu, size: 30),
+          Icon(Icons.perm_identity, size: 30),
+        ],
+        color: Colors.white,
+        buttonBackgroundColor: Colors.white,
+        backgroundColor: Colors.blue[100],
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 600),
+        onTap: (index) {
+          setState(() {
+            i = index;
+          });
+        },
+      ),
+    );
+  }
+}
