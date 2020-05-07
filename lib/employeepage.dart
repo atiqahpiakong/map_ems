7bea32f686e02e030878025aca09a19761fdca33
Balqesh <nurulbalqesh@gmail.com>
2020-05-08 05:29:24 +0800

    container for employees details

---
+++ lib/employeepage.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'model/employee.dart';
+import 'list_emp_details.dart';
+
+class EmpList extends StatefulWidget {
+  EmpList(this._employee);
+
+  final List _employee;
+  // final String title;
+
+  @override
+  _EmpListState createState() => _EmpListState();
+}
+
+class _EmpListState extends State<EmpList> {
+  List<Employee> data;
+
+  @override
+  Widget build(BuildContext context) {
+
+    
+    ListTile makeListTile(Employee emp) => ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 13.0, vertical: 7.0),
+
+          leading: Container(
+            padding: EdgeInsets.only(left: 30.0, right: 0.0),
+            child: CircleAvatar(
+              backgroundImage: ExactAssetImage('assets/img/profile.png'),
+              backgroundColor: Color(0xff3DBC93),
+              radius: 30,
+            ),
+          ),
+
+          title: Text(
+            emp.name,
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 15.0,
+                width: 300.0,
+                child: Container(
+                  child: Text(
+                    emp.dept,
+                    style: TextStyle(color: Colors.black, fontSize: 12.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => EmployeeProfile(emp: emp)));
+          },
+        );
+
+    Card makeCard(Employee emp) => Card(
+          elevation: 6.0,
+          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          child: Container(
+            decoration: BoxDecoration(color: Color(0xffECECEC)),
+            child: makeListTile(emp),
+          ),
+        );
+
+    final makeBody = Container(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: widget._employee.length,
+        itemBuilder: (BuildContext context, int index) {
+          return makeCard(widget._employee[index]);
+        },
+      ),
+    );
+
+    final topAppBar = AppBar(
+      elevation: 0,
+      brightness: Brightness.light,
+      iconTheme: IconThemeData(color: Colors.white),
+      backgroundColor: Color(0xff022264),
+      title: Text(
+        "LIST OF EMPLOYEE",
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: topAppBar,
+      body: makeBody,
+    );
+  }
+}
---
+++ lib/list_emp_details.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'model/employee.dart';
+
+class EmployeeProfile extends StatefulWidget {
+  final Employee emp;
+
+  EmployeeProfile({Key key, this.emp}) : super(key: key);
+
+  @override
+  _EmployeeProfileState createState() => _EmployeeProfileState();
+}
+
+class _EmployeeProfileState extends State<EmployeeProfile> {
+  var currentSelectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final name = Container(
+      width: MediaQuery.of(context).size.width * 10.0,
+      margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 30),
+      child: new Container(
+        width: 60,
+        padding: EdgeInsets.all(20.0),
+        decoration: new BoxDecoration(
+          color: Color(0xffECECEC),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: new RichText(
+          
+          text: TextSpan(
+            //style: DefaultTextStyle.of(context).style,
+            children: <TextSpan>[
+              TextSpan(
+                text: "Employee Name: ",
+                style: TextStyle(color: Color(0xFF000000), height: 1.4, fontWeight: FontWeight.bold),
+
+              ),
+
+              TextSpan(
+                text: widget.emp.name,
+                style: TextStyle(color: Color(0xFF000000), height: 1.4)
+
+              )
+
+            ],
+
+
+          
+      ),
+    )
+    )
+    );
+
+    final dept = Container(
+      width: MediaQuery.of(context).size.width * 90.0,
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 1),
+      padding: EdgeInsets.only(left: 20.0, right: 20, top: 10),
+      child: new Container(
+        width: 60,
+        padding: EdgeInsets.all(20.0),
+        decoration: new BoxDecoration(
+          color: Color(0xffECECEC),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+         child: new RichText(
+          
+          text: TextSpan(
+            //style: DefaultTextStyle.of(context).style,
+            children: <TextSpan>[
+              TextSpan(
+                text: "Department: ",
+                style: TextStyle(color: Color(0xFF000000), height: 1.4, fontWeight: FontWeight.bold),
+
+              ),
+
+              TextSpan(
+                text: widget.emp.dept,
+                style: TextStyle(color: Color(0xFF000000), height: 1.4)
+
+              )
+
+            ],
+
+
+          
+      ),
+    )
+      ),
+    );
+
+    final email = Container(
+      width: MediaQuery.of(context).size.width * 90.0,
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 1),
+      padding: EdgeInsets.only(left: 20.0, right: 20, top: 10),
+      child: new Container(
+        width: 60,
+        padding: EdgeInsets.all(20.0),
+        decoration: new BoxDecoration(
+          color: Color(0xffECECEC),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+         child: new RichText(
+          
+          text: TextSpan(
+            //style: DefaultTextStyle.of(context).style,
+            children: <TextSpan>[
+              TextSpan(
+                text: "Email: ",
+                style: TextStyle(color: Color(0xFF000000), height: 1.4, fontWeight: FontWeight.bold),
+
+              ),
+
+              TextSpan(
+                text: widget.emp.email,
+                style: TextStyle(color: Color(0xFF000000), height: 1.4)
+
+              )
+
+            ],
+
+
+          
+      ),
+    )
+      ),
+    );
+
+    final contact = Container(
+      width: MediaQuery.of(context).size.width * 90.0,
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 1),
+      padding: EdgeInsets.only(left: 20.0, right: 20, top: 10),
+      child: new Container(
+        width: 60,
+        padding: EdgeInsets.all(20.0),
+        decoration: new BoxDecoration(
+          color: Color(0xffECECEC),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: new RichText(
+          
+          text: TextSpan(
+            //style: DefaultTextStyle.of(context).style,
+            children: <TextSpan>[
+              TextSpan(
+                text: "Contact Number: ",
+                style: TextStyle(color: Color(0xFF000000), height: 1.4, fontWeight: FontWeight.bold),
+
+              ),
+
+              TextSpan(
+                text: widget.emp.contact,
+                style: TextStyle(color: Color(0xFF000000), height: 1.4)
+
+              )
+
+            ],
+
+
+          
+      ),
+    )
+      ),
+    );
+
+    final address = Container(
+      width: MediaQuery.of(context).size.width * 90.0,
+      margin: const EdgeInsets.only(left: 20.0, right: 20.0, top: 1),
+      padding: EdgeInsets.only(left: 20.0, right: 20, top: 10),
+      child: new Container(
+        width: 60,
+        padding: EdgeInsets.all(20.0),
+        decoration: new BoxDecoration(
+          color: Color(0xffECECEC),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: new RichText(
+          
+          text: TextSpan(
+            //style: DefaultTextStyle.of(context).style,
+            children: <TextSpan>[
+              TextSpan(
+                text: "Address: ",
+                style: TextStyle(color: Color(0xFF000000), height: 1.4, fontWeight: FontWeight.bold),
+
+              ),
+
+              TextSpan(
+                text: widget.emp.address,
+                style: TextStyle(color: Color(0xFF000000), height: 1.4)
+
+              )
+
+            ],
+
+
+          
+      ),
+    )
+      ),
+    );
+
+    final topAppBar = AppBar(
+      elevation: 0,
+      brightness: Brightness.light,
+      iconTheme: IconThemeData(color: Colors.white),
+      backgroundColor: Color(0xff022264),
+      title: Text(
+        "Employee Details",
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      centerTitle: true,
+    );
+
+     return Scaffold(
+        resizeToAvoidBottomPadding: true,
+        backgroundColor: Colors.white,
+        appBar: topAppBar,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[name, dept, email, contact, address],
+          ),
+        ));
+  }
+}
