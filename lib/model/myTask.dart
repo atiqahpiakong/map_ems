478decd493f426545b0abe861d7efbd4747b3398
Siti Nabilah <sitinabilah0123@gmail.com>
2020-05-07 15:32:26 +0800

    Create mockdata for task

---
+++ lib/model/myTask.dart
@@ -0,0 +1,9 @@
+class Task {
+  String due;
+  String subject;
+  String description;
+  String status;
+  String note;
+
+  Task({this.due, this.subject, this.description, this.status, this.note});
+}
