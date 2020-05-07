68e5e2993a62270040251bcc0b501ffeb82a8199
nurulain <nurulain0897@gmail.com>
2020-05-08 01:41:43 +0800

    Adding mock_data.dart in models

---
+++ lib/model/mock_data.dart
@@ -0,0 +1,31 @@
+import 'assessment.dart';
+import 'employees.dart';
+
+List<Assessment> mockData = [
+  Assessment(
+    Employees('SA1234', 'Siti Munirah', 'Manager'),
+    [4, 2, 3, 2, 4, 3, 2],
+  ),
+  Assessment(
+    Employees('BA3764', 'Loki Ahmad', 'System Admin'),
+    [3, 2, 3, 3, 3, 3, 4],
+  ),
+  Assessment(
+    Employees('KH8736', 'Kamal Adli', 'Software Developer'),
+    [2, 1, 3, 2, 4, 4, 2],
+  ),
+  Assessment(
+    Employees('FE8792', 'Iman Abdul', 'Finance'),
+    [4, 4, 4, 4, 4, 4, 3],
+  ),
+
+  Assessment(
+    Employees('WQ03973', 'Raisa Munirah', 'Human Resource'),
+    [2, 2, 3, 1, 1, 3, 4],
+  ),
+
+  Assessment(
+    Employees('VH7625', 'Mazlan Saad', 'Civil Engineer'),
+    [4, 2, 3, 3, 1, 4, 4],
+  ),
+];
