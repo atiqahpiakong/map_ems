468df877738b54f1d40c4adcfd21aceb2e0d317e
nurulain <nurulain0897@gmail.com>
2020-05-08 20:34:02 +0800

    add myperformance.dart

---
+++ lib/myperformance.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'CircleProgress.dart';
+
+class PerformancePage extends StatefulWidget {
+  PerformancePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _CircleProgressState createState() => _CircleProgressState();
+}
+
+class _CircleProgressState extends State<PerformancePage> with SingleTickerProviderStateMixin {
+
+  AnimationController progressController;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    progressController = AnimationController(vsync: this,duration: Duration(milliseconds: 1000));
+    animation = Tween<double>(begin: 0,end: 80).animate(progressController)..addListener((){
+      setState(() {
+
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: CustomPaint(
+
+          foregroundPainter: CircleProgress(animation.value), // this will add custom painter after child
+          child: Container(
+            width: 200,
+            height: 200,
+            child: GestureDetector(
+                onTap: (){
+                  if(animation.value == 80){
+                    progressController.reverse();
+                  }else {
+                    progressController.forward();
+                  }
+                },
+                child: Center(child: Text("${animation.value.toInt()} %",style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                ),))),
+          ),
+
+        
+        ),
+
+      ),
+    );
+  }
+}
