3696e861adb833598d6f3304f46dbac0ac42dc9e
atiqah <atiqahpkg@gmail.com>
2020-05-08 10:36:14 +0800

    edit leave mock data

--- lib/model/Leave.dart
+++ lib/model/Leave.dart
@@ -1,45 +1,39 @@
 import 'myLeave.dart';
 
-List <Leave> mockLeave = [
-
-    Leave(
-        name: "Nurul Atiqah",
-        dateFrom: "3/10/2020",
-        dateTo: "4/10/2020",
-        status: "Pending",
-        type: "Sick",
-        reason: "Fever"),
-        
-    Leave(
-        name: "Nurul Atiqah",
-        dateFrom: "23/02/2020",
-        dateTo: "24/02/2020",
-        status: "Pending",
-        type: "Vacation",
-        reason: "Emergency"),
-        
-    Leave(
-        name: "Nurul Atiqah",
-        dateFrom: "3/10/2019",
-        dateTo: "4/10/2019",
-        status: "Pending",
-        type: "Holiday",
-        reason: "friends"),
-
-    Leave(
-        name: "Nurul Atiqah",
-        dateFrom: "1/10/2019",
-        dateTo: "2/10/2019",
-        status: "Rejected",
-        type: "Holiday",
-        reason: "Balik Kampung"),
-
-     Leave(
-
-        name: "Siti Nabilah",
-        dateFrom: "1/09/2019",
-        dateTo: "4/09/2019",
-        status: "Approved",
-        type: "Married",
-        reason: "Balik Kampung"),
-  ];
+List<Leave> mockLeave = [
+  Leave(
+      name: "Nurul Atiqah",
+      dateFrom: "3/10/2020",
+      dateTo: "4/10/2020",
+      status: "Pending",
+      type: "Medical Leave",
+      reason: "Fever"),
+  Leave(
+      name: "Nurul Atiqah",
+      dateFrom: "23/02/2020",
+      dateTo: "24/02/2020",
+      status: "Pending",
+      type: "Medical Leave",
+      reason: "Emergency"),
+  Leave(
+      name: "Nurul Atiqah",
+      dateFrom: "3/10/2019",
+      dateTo: "4/10/2019",
+      status: "Pending",
+      type: "Annual Leave",
+      reason: "Holiday"),
+  Leave(
+      name: "Nurul Atiqah",
+      dateFrom: "1/10/2019",
+      dateTo: "2/10/2019",
+      status: "Rejected",
+      type: "Annual Leave",
+      reason: "Balik Kampung"),
+  Leave(
+      name: "Siti Nabilah",
+      dateFrom: "1/09/2019",
+      dateTo: "4/09/2019",
+      status: "Approved",
+      type: "Annual Leave",
+      reason: "Balik Kampung"),
+];
