39ccc21410eabdcec11040db689e533cbdd093e0
atiqah <atiqahpkg@gmail.com>
2020-05-08 11:27:40 +0800

    task form code refractor + set page at task list

--- lib/task_Manager.dart
+++ lib/task_Manager.dart
@@ -1,9 +1,8 @@
 import 'package:emsproject/model/myTask.dart';
+import 'package:emsproject/taskform.dart';
 import 'package:flutter/material.dart';
 import 'package:emsproject/details_PendingTask.dart';
 import 'package:emsproject/details_CompletedTask.dart';
-
-
 
 class ManagerTaskPage extends StatefulWidget {
   ManagerTaskPage(this._task);
@@ -15,75 +14,68 @@ class ManagerTaskPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ManagerTaskPage> {
- 
-
   @override
   Widget build(BuildContext context) {
-
-
     ListTile makeListTile(Task task) => ListTile(
-      contentPadding: EdgeInsets.symmetric(horizontal: 9.0, vertical: 7.0),
-      leading: Container(
-        padding: EdgeInsets.only(left: 20.0, right:0.0),
-        child: CircleAvatar(
-          backgroundImage: ExactAssetImage('assets/img/profile.png'),
-          backgroundColor: Color(0xff3DBC93), 
-          radius: 30, 
-        ),
-      ),
-    
-      subtitle: Column (
-        crossAxisAlignment: CrossAxisAlignment.start,
-
-        children: <Widget>[
-          Text ("Due : " + task.due,
-           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-
-          SizedBox(height:4,),
-          
-          Text ("Subject : " + task.subject, 
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-
-          SizedBox(height:10,),
-
-          SizedBox(
-            height: 15.0,
-            width: 80.0,
-
-            child: Container(
-              color: task.status == "Completed" ? Color(0xffB0EFCB) : Color(0xffEFB0C3),  
-              child: Center(
-                child:Text(task.status,
-                  style: TextStyle(color: Colors.black, fontSize: 12.0),
+          contentPadding: EdgeInsets.symmetric(horizontal: 9.0, vertical: 7.0),
+          leading: Container(
+            padding: EdgeInsets.only(left: 20.0, right: 0.0),
+            child: CircleAvatar(
+              backgroundImage: ExactAssetImage('assets/img/profile.png'),
+              backgroundColor: Color(0xff3DBC93),
+              radius: 30,
+            ),
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text("Due : " + task.due,
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 4,
+              ),
+              Text("Subject : " + task.subject,
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 15.0,
+                width: 80.0,
+                child: Container(
+                  color: task.status == "Completed"
+                      ? Color(0xffB0EFCB)
+                      : Color(0xffEFB0C3),
+                  child: Center(
+                    child: Text(
+                      task.status,
+                      style: TextStyle(color: Colors.black, fontSize: 12.0),
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),          
-        ],
-      ),
-    
-    onTap: () {
-
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => DetailCompletedTaskPage(task: task)));
-     
-    },
-    );
+            ],
+          ),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DetailCompletedTaskPage(task: task)));
+          },
+        );
 
     Card makeCard(Task task) => Card(
-      elevation: 8.0,
-      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-
-      child: Container(
-        decoration: BoxDecoration(color: Color(0xffECECEC)),
-        child: makeListTile(task),
-      ),
-    );
+          elevation: 8.0,
+          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+          child: Container(
+            decoration: BoxDecoration(color: Color(0xffECECEC)),
+            child: makeListTile(task),
+          ),
+        );
 
     final makeBody = SingleChildScrollView(
-      
       child: ListView.builder(
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
@@ -99,18 +91,19 @@ class _ListPageState extends State<ManagerTaskPage> {
       brightness: Brightness.light,
       iconTheme: IconThemeData(color: Colors.white),
       backgroundColor: Color(0xff022264),
-
-      title: Text("ALL TASK",
-              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+      title: Text(
+        "ALL TASK",
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
       actions: <Widget>[
-          IconButton(icon: Icon(Icons.add_circle_outline ,color: Colors.white, size:30),onPressed: (){
-          Navigator.of(context)
-                .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-              return null;
-            }));
-
-        },),],
-        
+        IconButton(
+          icon: Icon(Icons.add_circle_outline, color: Colors.white, size: 30),
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => TaskFormPage()));
+          },
+        ),
+      ],
       centerTitle: true,
     );
 
@@ -118,7 +111,6 @@ class _ListPageState extends State<ManagerTaskPage> {
       backgroundColor: Colors.blue[100],
       appBar: topAppBar,
       body: makeBody,
-      
     );
   }
 }
--- lib/taskform.dart
+++ lib/taskform.dart
@@ -10,7 +10,6 @@ class TaskFormPage extends StatefulWidget {
 }
 
 class _TaskFormPage extends State<TaskFormPage> {
-
   DateTime _submitDate;
 
   var myFormat = DateFormat('yyyy-MM-dd');
@@ -27,6 +26,142 @@ class _TaskFormPage extends State<TaskFormPage> {
     "  Miechel"
   ];
 
+  Widget _buildSubjectBox() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: new Container(
+        width: 360,
+        padding: EdgeInsets.all(10.0),
+        decoration: new BoxDecoration(
+          color: Color(0xffECECEC),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: new TextFormField(
+          // validator: SubjectValidator.validate,
+          decoration: new InputDecoration(
+            hintText: " Subject ",
+            fillColor: Color(0xffECECEC),
+            filled: true,
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                borderSide: BorderSide(color: Color(0xffECECEC))),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAssignTo() {
+    return Container(
+        width: MediaQuery.of(context).size.width * 40.0,
+        margin:
+            const EdgeInsets.only(left: 20.0, right: 20.0, top: 10, bottom: 10),
+        child: new Container(
+            padding: EdgeInsets.all(10.0),
+            decoration: new BoxDecoration(
+              color: Color(0xffECECEC),
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton(
+                  hint: Text('   Assign To'),
+                  value: _valFriends,
+                  items: _employeeNames.map((value) {
+                    return DropdownMenuItem(
+                      child: Text(value),
+                      value: value,
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _valFriends = value;
+                    });
+                  }),
+            )));
+  }
+
+  Widget _buildDescription() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: new Container(
+        width: 360,
+        padding: EdgeInsets.all(10.0),
+        decoration: new BoxDecoration(
+          color: Color(0xffECECEC),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: new TextFormField(
+          // validator: DescriptionValidator.validate,
+          decoration: new InputDecoration(
+            hintText: " Description ",
+            fillColor: Color(0xffECECEC),
+            filled: true,
+            enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15.0),
+                borderSide: BorderSide(color: Color(0xffECECEC))),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDueOnBox() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: new Container(
+        width: 360,
+        padding: EdgeInsets.all(10.0),
+        decoration: new BoxDecoration(
+          color: Color(0xffECECEC),
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Center(
+          child: Text(
+            _submitDate == null ? ' Due On' : myFormat.format(_submitDate),
+            style: TextStyle(
+                color: Colors.grey, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildChooseDateButton() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 2.0, bottom: 20.0),
+      child: RaisedButton(
+        child: Text('Choose Date'),
+        color: Colors.blue,
+        splashColor: Color.fromRGBO(10, 20, 10, 10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        onPressed: () {
+          showDatePicker(
+                  context: context,
+                  initialDate:
+                      _submitDate == null ? DateTime.now() : _submitDate,
+                  firstDate: DateTime(2001),
+                  lastDate: DateTime(2222))
+              .then((date) {
+            setState(() {
+              _submitDate = date;
+            });
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return FloatingActionButton.extended(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      backgroundColor: Colors.lightGreen,
+      label: Text('Submit'),
+      icon: Icon(Icons.check_circle),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,131 +177,12 @@ class _TaskFormPage extends State<TaskFormPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: new Container(
-                width: 360,
-                padding: EdgeInsets.all(10.0),
-                decoration: new BoxDecoration(
-                  color: Color(0xffECECEC),
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: new TextFormField(
-                  // validator: SubjectValidator.validate,
-                  decoration: new InputDecoration(
-                    hintText: " Subject ",
-                    fillColor: Color(0xffECECEC),
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                        borderSide: BorderSide(color: Color(0xffECECEC))),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-                width: MediaQuery.of(context).size.width * 40.0,
-                margin: const EdgeInsets.only(
-                    left: 20.0, right: 20.0, top: 10, bottom: 10),
-                child: new Container(
-                    padding: EdgeInsets.all(10.0),
-                    decoration: new BoxDecoration(
-                      color: Color(0xffECECEC),
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton(
-                          hint: Text('   Assign To'),
-                          value: _valFriends,
-                          items: _employeeNames.map((value) {
-                            return DropdownMenuItem(
-                              child: Text(value),
-                              value: value,
-                            );
-                          }).toList(),
-                          onChanged: (value) {
-                            setState(() {
-                              _valFriends = value;
-                            });
-                          }),
-                    ))),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: new Container(
-                width: 360,
-                padding: EdgeInsets.all(10.0),
-                decoration: new BoxDecoration(
-                  color: Color(0xffECECEC),
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: new TextFormField(
-                  // validator: DescriptionValidator.validate,
-                  decoration: new InputDecoration(
-                    hintText: " Description ",
-                    fillColor: Color(0xffECECEC),
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                        borderSide: BorderSide(color: Color(0xffECECEC))),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: new Container(
-                width: 360,
-                padding: EdgeInsets.all(10.0),
-                decoration: new BoxDecoration(
-                  color: Color(0xffECECEC),
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: Center(
-                  child: Text(
-                    _submitDate == null
-                        ? ' Due On'
-                        : myFormat.format(_submitDate),
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 2.0, bottom: 20.0),
-              child: RaisedButton(
-                child: Text('Choose Date'),
-                color: Colors.blue,
-                splashColor: Color.fromRGBO(10, 20, 10, 10),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30)),
-                onPressed: () {
-                  showDatePicker(
-                          context: context,
-                          initialDate: _submitDate == null
-                              ? DateTime.now()
-                              : _submitDate,
-                          firstDate: DateTime(2001),
-                          lastDate: DateTime(2222))
-                      .then((date) {
-                    setState(() {
-                      _submitDate = date;
-                    });
-                  });
-                },
-              ),
-            ),
-
-             FloatingActionButton.extended(
-              onPressed: () {
-              },
-              backgroundColor: Colors.lightGreen,
-              label: Text('Submit'),
-              icon: Icon(Icons.check_circle),
-            )
-
+            _buildSubjectBox(),
+            _buildAssignTo(),
+            _buildDescription(),
+            _buildDueOnBox(),
+            _buildChooseDateButton(),
+            _buildSubmitButton(),
           ],
         ),
       ),
