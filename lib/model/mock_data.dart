08be58929b3066390e5b29c7dc0647b129f4fbe4
nurulain <nurulain0897@gmail.com>
2020-05-09 15:13:39 +0800

    modify performance page

--- lib/model/mock_data.dart
+++ lib/model/mock_data.dart
@@ -4,28 +4,28 @@ import 'employees.dart';
 List<Assessment> mockData = [
   Assessment(
     Employees('SA1234', 'Siti Munirah', 'Manager'),
-    [4, 2, 3, 2, 4, 3, 2],
+    [0, 0, 0, 0, 0, 0, 0],
   ),
   Assessment(
     Employees('BA3764', 'Loki Ahmad', 'System Admin'),
-    [3, 2, 3, 3, 3, 3, 4],
+    [0, 0, 0, 0, 0, 0, 0],
   ),
   Assessment(
     Employees('KH8736', 'Kamal Adli', 'Software Developer'),
-    [2, 1, 3, 2, 4, 4, 2],
+    [0, 0, 0, 0, 0, 0, 0],
   ),
   Assessment(
     Employees('FE8792', 'Iman Abdul', 'Finance'),
-    [4, 4, 4, 4, 4, 4, 3],
+    [0, 0, 0, 0, 0, 0, 0],
   ),
 
   Assessment(
     Employees('WQ03973', 'Raisa Munirah', 'Human Resource'),
-    [2, 2, 3, 1, 1, 3, 4],
+    [0, 0, 0, 0, 0, 0, 0],
   ),
 
   Assessment(
     Employees('VH7625', 'Mazlan Saad', 'Civil Engineer'),
-    [4, 2, 3, 3, 1, 4, 4],
+    [0, 0, 0, 0, 0, 0, 0],
   ),
 ];
