b1bc951c9cd231e88116a9be8c0910a83c6afe9a
Nurul Balqesh <nurulbalqesh@gmail.com>
2020-07-23 13:58:49 +0800

    user view profile page

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'editprofile.dart';
-import 'package:emsproject/model/empmockdata.dart';
-import 'package:emsproject/model/employee.dart';
+import 'package:emsproject/model/user_model.dart';
+import './services/user_data_service.dart';
+
 //import 'homescreen_admin.dart';
 
 class ProfilePage extends StatefulWidget {
-  final Employee emp;
-  ProfilePage(this.emp);
+  // final Employee emp;
+  // ProfilePage(this.emp);
 
   @override
   _ProfilePageState createState() => _ProfilePageState();
@@ -14,220 +15,34 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
 
+  List<User> _user;
+  final dataService = UserDataService();
 
-  Widget _greenColors() {
-    return Positioned(
-      top: 0,
-      child: Container(
-        color: Color(0xff022264),
-        //height: 205,
-        width: MediaQuery.of(context).size.width,
-
-        padding: const EdgeInsets.all(14),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                 CircleAvatar(
-                   backgroundImage: ExactAssetImage('assets/img/profile.png'),
-                   backgroundColor: Color(0xff3DBC93), 
-                    radius: 60,
-                  
-                )
-                                  
-                ],
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                  widget.emp.name, textAlign: TextAlign.center,
-                  
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                   
-                  ),
-                ),
-                ],
-              ),
-
-               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                  widget.emp.dept, textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    color: Colors.white,
-                    
-                    
-                  ),
-                ),
-                ],
-              ),
-            ],
-          ),
-      ),
-    );
-  }
-
-  Widget _userPhone() {
-    return Positioned(
-      top: 205,
-      child: Container(
-        margin: EdgeInsets.all(18),
-        
-        width: MediaQuery.of(context).size.width * 0.90,
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(20)),
-        child: Container(
-          padding: EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                
-              Icon(
-                  Icons.call,
-                  color: Colors.black,
-                ),
-         
-        
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                  widget.emp.contact, 
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
-                    
-                    
-                  ),
-                ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-   Widget _userEmail() {
-    return Positioned(
-      top: 270,
-      child: Container(
-        margin: EdgeInsets.all(18),
-        
-        width: MediaQuery.of(context).size.width * 0.90,
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(20)),
-        
-        child: Container(
-          padding: EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                
-              Icon(
-                  Icons.email,
-                  color: Colors.black,
-                ),
-         
-        
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                  widget.emp.email, 
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
-                    
-                    
-                  ),
-                ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _userAddress() {
-    return Positioned(
-      top: 335,
-      child: Container(
-        margin: EdgeInsets.all(18),
-        
-        width: MediaQuery.of(context).size.width * 0.90,
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(20)),
-        
-        child: Container(
-          padding: EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                
-              Icon(
-                  Icons.location_on,
-                  color: Colors.black,
-                ),
-         
-        
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Flexible(
-                    child:Text(
-                  widget.emp.address, 
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
-                    
-                    
-                  ),)
-                ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-  @override
+   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<List<User>>(
+        future: dataService.getMyProfile(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            _user = snapshot.data;
+            return _buildMainScreen(snapshot);
+          }
+          return _buildFetchingDataScreen();
+        });
+  }
+
+
+  Scaffold _buildMainScreen(snapshot) {
+   
     return Scaffold(
-      
       appBar: AppBar(
-        
         brightness: Brightness.light,
-        
+
         title: Text(
           "PROFILE",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        
+
         elevation: 0,
         backgroundColor: Color(0xff022264),
         centerTitle: true,
@@ -241,13 +56,13 @@ class _ProfilePageState extends State<ProfilePage> {
         // ),
         actions: <Widget>[
           
-          IconButton(icon: Icon(Icons.edit, color: Colors.white),onPressed: (){
-          Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => EditProfile(widget.emp)));
-
-        },),],
+          IconButton(
+            icon: Icon(Icons.edit, color: Colors.white),
+            onPressed: () {
+             
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Stack(
@@ -257,21 +72,193 @@ class _ProfilePageState extends State<ProfilePage> {
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
             ),
-            _greenColors(),
-            _userPhone(),
-            _userEmail(),
-            _userAddress()
+            Positioned(
+              top: 0,
+              child: Container(
+                color: Color(0xff022264),
+                //height: 205,
+                width: MediaQuery.of(context).size.width,
 
+                padding: const EdgeInsets.all(14),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage:
+                              ExactAssetImage('assets/img/profile.png'),
+                          backgroundColor: Color(0xff3DBC93),
+                          radius: 60,
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          //'${_user.name}',
+                          _user[0].name,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                           _user[0].department,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 205,
+              child: Container(
+                margin: EdgeInsets.all(18),
+                width: MediaQuery.of(context).size.width * 0.90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(
+                            Icons.call,
+                            color: Colors.black,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                             _user[0].phone,
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 270,
+              child: Container(
+                margin: EdgeInsets.all(18),
+                width: MediaQuery.of(context).size.width * 0.90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(
+                            Icons.email,
+                            color: Colors.black,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                             _user[0].email,
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 335,
+              child: Container(
+                margin: EdgeInsets.all(18),
+                width: MediaQuery.of(context).size.width * 0.90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.black,
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Flexible(
+                              child: Text(
+                             _user[0].address,
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
           ],
         ),
-
-      
-
-
       ),
     );
-    
   }
 
-  
+   Scaffold _buildFetchingDataScreen() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircularProgressIndicator(),
+            SizedBox(height: 50),
+            Text('Fetching data... Please wait'),
+          ],
+        ),
+      ),
+    );
+  }
 }
